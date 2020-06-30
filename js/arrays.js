549d23a52d8454a6cd7a4a77dfa23cc14b087743
Amaro Terrazas <42896111+AmaroT@users.noreply.github.com>
2020-06-30 16:00:20 -0500

    updated end of day progress

---
+++ js/arrays.js
@@ -0,0 +1,28 @@
+// var myarr = [5,"Sam", true];
+// console.log(myarr);
+// var myarr = ["String1","stringtwo",1, ["1","2",3]];
+// console.log(myarr);
+// console.log(myarr.length);
+// var shapes = ['square', 'rectangle', 'circle', 'triangle'];
+// var notice = 'The shape at index 1 is: ' + shapes[1] + "\n";
+// notice = notice + 'The shape at index 4 is: ' + shapes[4];
+// alert(notice);
+// fruits = ["Banana", "Orange", "Apple", "Mango"];
+// for(i = 0; i > 0; i++);
+// console.log(fruits[i++]);
+
+// fruits = ["Banana", "Orange", "Apple", "Mango"];
+// for( var i = 0;i < fruits.length;i++);
+// console.log("The fruit at index " + i + "is" + fruits[i] + ".");
+fruits = ["Banana", "Orange", "Apple", "Mango"];
+// for (var i = 0; i < fruits.length; i++) {
+//     console.log("The fruit at index " + i + " is " + fruits[i] + ".");
+// }
+
+fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.forEach(function(fruit, index){
+    console.log("the fruit at index " + index + " is " + fruit + ".");
+});
+// function myTwoParamFunction(shape, index){
+//     alert("the shape at index " + index + " is " + shape + ".");
+// }
