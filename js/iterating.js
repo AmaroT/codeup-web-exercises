122782ddee59c4af276e0dc540bd87b7a521b142
Amaro Terrazas <42896111+AmaroT@users.noreply.github.com>
2020-06-30 21:23:51 -0500

    updated end of day

--- js/iterating.js
+++ js/iterating.js
@@ -1,4 +1,4 @@
-(function(){
+(function() {
     "use strict";
 
     /**
@@ -6,30 +6,38 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
+    var names = ["John", "Sam", "Jose", "Juan"];
 
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
-
+    console.log(names.length);
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
+    console.log(names);
 
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-
+    for (var i = 0; i < names.length; i++) {
+        console.log("The name at index " + i + " is " + names[i] + ".");
+    }
+    ;
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
 
+    names.forEach(function (names) {
+        console.log("FOR-EACH: The name" + " is " + names + ".");
+    });
     /**
      * TODO:
      * Create the following three functions, each will accept an array and
@@ -43,5 +51,11 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+
+
+    var numbers = [1, 2, 3, 4, 5];
+    console.log(numbers[0]);
+    console.log(numbers[1]);
+    console.log(numbers[4]);
 
 })();
