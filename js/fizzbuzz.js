46687081d208231ae46e70c968ce3d573c6dde13
Amaro Terrazas <42896111+AmaroT@users.noreply.github.com>
2020-07-07 09:25:36 -0500

    fizzbuzz practice

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -17,4 +17,20 @@
     }
     fizzbuzz(23);
 
+    let fizzbuzz2 = function(num){
+        for (let i = 1; i <=num; i++ ){
+            //if both are div by 3 and 5
+            if(i % 3 == 0 && i % 5 == 0){
+                console.log('fizzbuzz');
+            }else if (i % 3 == 0){
+                console.log('fizz');
+            }else if(i % 5 == 0){
+                console.log('buzz');
+            }else{
+                console.log(i);
+            }
+        }
+    }
+    fizzbuzz2(45);
+
 })();
