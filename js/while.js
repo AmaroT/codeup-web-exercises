48225375095e3b1b96ebf44e7728e25a06bc34c1
Amaro Terrazas <42896111+AmaroT@users.noreply.github.com>
2020-07-16 17:30:54 -0500

    daily push, init jquery"

--- js/while.js
+++ js/while.js
@@ -4,27 +4,27 @@
 //     i = i * 2;
 //     console.log(i);
 // }
-
+//
 // do while
 //
-
-    // var allCones = Math.floor(Math.random() * 50) + 50;
-    // do {
-    //     var conesBought = Math.floor(Math.random() * 5) + 5;
-    //     while (allCones > 0) {
-    //         if (conesBought < allCones) {
-    //             var enoughCones = conesBought + " cones sold." + "I have " + allCones + " cones left.";
-    //             console.log(enoughCones);
-    //         } else {
-    //             var notEnoughCones = " I can not sell you " + conesBought + " cones." + " I only have " + allCones + " left.";
-    //             console.log(notEnoughCones);
-    //         }
-    //         conesBought++;
-    //     }
-    //
-    // }while(allCones = 0);
-    //     console.log("Yay! Sold them all!")
-
+//
+//     var allCones = Math.floor(Math.random() * 50) + 50;
+//     do {
+//         var conesBought = Math.floor(Math.random() * 5) + 5;
+//         while (allCones > 0) {
+//             if (conesBought < allCones) {
+//                 var enoughCones = conesBought + " cones sold." + "I have " + allCones + " cones left.";
+//                 console.log(enoughCones);
+//             } else {
+//                 var notEnoughCones = " I can not sell you " + conesBought + " cones." + " I only have " + allCones + " left.";
+//                 console.log(notEnoughCones);
+//             }
+//             conesBought++;
+//         }
+//
+//     }while(allCones = 0);
+//         console.log("Yay! Sold them all!")
+//
 // var allCones = Math.floor(Math.random() * 50) + 50;
 // do{
 //     while( allCones > 0){
@@ -39,12 +39,12 @@
 //
 // }while (allCones = 0);
 // console.log("Yay! sold them all!");
-
-
+//
+//
 // 5 cones sold...  // if there are enough cones
 // Cannot sell you 6 cones I only have 3...  // If there are not enough cones
 //     Yay! I sold them all! // If there are no more cones
-
+//
 // var allCones = Math.floor(Math.random() * 50) + 50;
 // do {
 //     let cones = Math.floor(Math.random()* 5) + 1;
